8e8963b02e259d2e7500906b6689ca67b3b6d31a
kszymon <kaniaszymon1@gmail.com>
2026-05-23 13:41:23 +0200

    add chapter parameterized_tests

---
+++ 09_parameterized_tests/02_project/tests/test_calc_math.py
@@ -0,0 +1,16 @@
+import unittest
+from parameterized import parameterized
+from calculator.calc_math import SimpleMathCalculator
+
+class TestSimpleMathCalculator(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = SimpleMathCalculator()
+
+    @parameterized.expand([
+        ('negative', -3, -2, -5),
+        ('mixed', -3,  2, -1),
+        ('positive', 3, 2, 5)
+    ])
+    def test_add(self,name, x, y, expected):
+        self.assertEqual(self.calc.add(x, y), expected)
